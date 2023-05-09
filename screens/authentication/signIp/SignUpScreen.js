59d98b8177c4a2418c64971886eaba815f5e4f66
MiqayelChaloyan <miqochaloyan@gmail.com>
2023-05-10 01:37:34 +0400

    In progress sign up

--- screens/authentication/signIp/SignUpScreen.js
+++ screens/authentication/signIp/SignUpScreen.js
@@ -1,10 +1,32 @@
-import {useState} from 'react';
-import {Text, View, StyleSheet, SafeAreaView, TextInput} from 'react-native';
-import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import React from 'react'
+import { Text, View, StyleSheet, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+// import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import { Formik, useFormik } from 'formik';
+import { signUpValidationSchema } from './signUpValidationSchema';
+
 
 const SignUpScreen = () => {
-  const [text, onChangeText] = useState('');
-  const [isSelected, setSelection] = useState(false);
+
+  const {
+    values,
+    handleChange,
+    errors,
+    setFieldTouched,
+    touched,
+    isValid,
+    handleSubmit,
+  } = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+    signUpValidationSchema,
+    onSubmit: data => {
+      console.log(data);
+    },
+  });
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -20,45 +42,71 @@ const SignUpScreen = () => {
           </View>
         </View>
       </View>
-      <View
-        style={{
-          flex: 2,
-          backgroundColor: 'pink',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: 15,
+
+      <Formik>
+        <View style={{
+          justifyContent: 'center', alignItems: 'center', gap: 15
         }}>
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeText}
-          value={text}
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeText}
-          value={text}
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeText}
-          value={text}
-        />
-        <View>
-          <BouncyCheckbox
-            size={25}
-            fillColor="red"
-            unfillColor="#FFFFFF"
-            text="Custom Checkbox"
-            iconStyle={{borderColor: 'red'}}
-            innerIconStyle={{borderWidth: 2}}
-            // textStyle={{fontFamily: 'JosefinSans-Regular'}}
-            onPress={isChecked => {}}
+          <TextInput
+            name="name"
+            placeholder="Name"
+            style={styles.input}
+            variant="standard"
+            onChangeText={handleChange('name')}
+            onBlur={() => setFieldTouched('name')}
+            value={values.name}
           />
-          <Text style={styles.label}>Do you like React Native?</Text>
+          {touched.name && errors.name && (
+            <Text style={styles.inputError}>{errors.name}</Text>
+          )}
+          <TextInput
+            name="email"
+            placeholder="Email Address"
+            style={styles.input}
+            variant="standard"
+            onChangeText={handleChange('email')}
+            onBlur={() => setFieldTouched('email')}
+            value={values.email}
+          // keyboardType="email-address"
+          />
+          {touched.email && errors.email && (
+            <Text style={styles.inputError}>{errors.email}</Text>
+          )}
+          <TextInput
+            name="password"
+            placeholder="Password"
+            style={styles.input}
+            variant="standard"
+            onChangeText={handleChange('password')}
+            onBlur={() => setFieldTouched('password')}
+            value={values.password}
+            secureTextEntry
+          />
+          {touched.password && errors.password && (
+            <Text style={styles.inputError}>{errors.password}</Text>
+          )}
+        </View>
+      </Formik>
+
+      <View style={{ alignItems: 'center' }}>
+        <View style={styles.button}>
+          <TouchableOpacity
+            disabled={!isValid}
+            onPress={() => handleSubmit()}>
+            <Text style={styles.buttonText}>Sign Up</Text>
+          </TouchableOpacity>
         </View>
       </View>
-      <View style={{flex: 3, backgroundColor: 'green'}} />
-    </SafeAreaView>
+
+      <View style={{ alignItems: 'center', height: '100%' }}>
+        <TouchableOpacity>
+          <Text style={{
+            fontSize: 20, color: '#5DB075',
+          }}>Forgot your password?</Text>
+        </TouchableOpacity>
+      </View>
+
+    </SafeAreaView >
   );
 };
 
@@ -69,17 +117,17 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   header: {
-    // backgroundColor: 'pink',
-    height: '15%',
     width: '100%',
+    height: '2%',
+    marginTop: '14%',
   },
   headerNavigate: {
-    flex: 1,
+    // flex: 1,
     flexDirection: 'row',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'space-between',
     marginTop: '4%',
-    marginHorizontal: 15,
+    marginHorizontal: 20,
   },
   option_1: {
     // width: '40%',
@@ -111,8 +159,8 @@ const styles = StyleSheet.create({
     textAlign: 'right',
   },
   input: {
-    width: 343,
-    height: 50,
+    width: '90%',
+    height: '15%',
     // margin: 5,
     borderWidth: 1,
     borderColor: '#E8E8E8',
@@ -122,6 +170,35 @@ const styles = StyleSheet.create({
   },
   checkbox: {
     alignSelf: 'center',
+  },
+
+  loginContainer: {
+    width: '80%',
+    height: 400,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    elevation: 10,
+    backgroundColor: '#e6e6e6'
+  },
+
+  inputError: {
+    color: 'blue',
+    fontSize: 50
+  },
+  button: {
+    marginTop: '21%',
+    width: '90%',
+    height: '20%',
+    backgroundColor: '#5DB075',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 100,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: "white"
   },
 });
 
