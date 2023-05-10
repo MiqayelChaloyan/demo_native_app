b0a16679d1c2a9aecee07bf323dea126df80634c
MiqayelChaloyan <miqochaloyan@gmail.com>
2023-05-10 18:14:10 +0400

    In process login and sign up pages

--- navigation/StackNavigation.jsx
+++ navigation/StackNavigation.jsx
@@ -1,58 +1,39 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ContentScreen from '../screens/Content/ContentScreen';
 import FeedScreen from '../screens/Feed/FeedScreen';
 import MarketScreen from '../screens/Market/MarketScreen';
 
 
-
-
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigation() {
-
-
-    return (
-        <>
-
-
-            <NavigationContainer>
-
-
-                <Stack.Navigator >
-
-
-                    <Stack.Screen
-                        name="Content"
-                        component={ContentScreen}
-                        options={{ headerShown: false }}
-
-                    />
-
-
-                    <Stack.Screen
-                        name="Feed"
-                        component={FeedScreen}
-                        options={{
-                            headerStyle: { backgroundColor: 'coral' }
-                        }} />
-
-                    <Stack.Screen
-                        name="Market"
-                        component={MarketScreen}
-                        options={{
-                            headerStyle: { backgroundColor: 'aqua' }
-                        }} />
-
-
-
-                </Stack.Navigator>
-
-            </NavigationContainer>
-
-        </>
-    );
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Content"
+            component={ContentScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Feed"
+            component={FeedScreen}
+            options={{
+              headerStyle: {backgroundColor: 'coral'},
+            }}
+          />
+          <Stack.Screen
+            name="Market"
+            component={MarketScreen}
+            options={{
+              headerStyle: {backgroundColor: 'aqua'},
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
-
-
