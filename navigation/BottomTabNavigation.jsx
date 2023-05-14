8bcfad03195b085550b2afcbcfd8bad4eba8cd3c
MiqayelChaloyan <miqochaloyan@gmail.com>
2023-05-14 23:39:25 +0400

    Added chat support

--- navigation/BottomTabNavigation.jsx
+++ navigation/BottomTabNavigation.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FeedScreen from '../screens/Feed/FeedScreen';
 import Profile from '../screens/Profile/Profile';
 
 // icons tabs
 import FeedIcon from '../assets/icons/Feed.svg';
 import ProfileIcon from '../assets/icons/Profile.svg';
+import MessagesIcon from '../assets/icons/Messages.svg';
+
 import StackNavigation from './StackNavigation';
+import MessagesList from '../screens/Messages/MessagesList';
 
 const Tab = createBottomTabNavigator();
 
@@ -25,8 +28,22 @@ export default function BottomTabNavigation() {
         component={StackNavigation}
         options={{
           title: '',
-          tabBarIcon: ({tabInfo, focused}) => (
+          tabBarIcon: ({ tabInfo, focused }) => (
             <FeedIcon
+              width={28}
+              height={28}
+              fill={focused ? '#5DB075' : '#BDBDBD'}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Messages"
+        component={MessagesList}
+        options={{
+          title: '',
+          tabBarIcon: ({ tabInfo, focused }) => (
+            <MessagesIcon
               width={28}
               height={28}
               fill={focused ? '#5DB075' : '#BDBDBD'}
@@ -39,7 +56,7 @@ export default function BottomTabNavigation() {
         component={Profile}
         options={{
           title: '',
-          tabBarIcon: ({tabInfo, focused}) => (
+          tabBarIcon: ({ tabInfo, focused }) => (
             <ProfileIcon
               width={28}
               height={28}
---
+++ screens/Messages/MessagesList.jsx
@@ -0,0 +1,116 @@
+import { useEffect, useRef, useState } from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    ScrollView,
+    FlatList,
+} from 'react-native';
+import Message from './Message';
+import ArrowIcon from '../../assets/icons/Arrow.svg';
+import styles from './style';
+
+
+const MessagesList = () => {
+    const [value, setValue] = useState('')
+    const user = useRef(0);
+    const scrollWiew = useRef();
+    const [messages, setMessages] = useState([
+        {
+            user: 1,
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla quam eu faci lisis mollis. "
+        },
+        {
+            user: 0,
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+        },
+        {
+            user: 0,
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+        },
+        {
+            user: 1,
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla quam eu faci lisis mollis. "
+        },
+    ]);
+
+    useEffect(() => {
+        setValue('');
+    }, [messages.length])
+
+    const getMessage = () => {
+        setMessages([
+            ...messages,
+            {
+                user: 0,
+                content: value
+            }]);
+    }
+
+    return (
+        <View style={styles.root}>
+            <View style={styles.messageProfile}>
+                <View style={styles.messegeTextContainer}>
+                    <TouchableOpacity>
+                        <Text style={styles.messegeText}>Back</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <Text style={styles.messege}>Messages</Text>
+                </View>
+                <View style={styles.messegeTextContainer}>
+                    <TouchableOpacity>
+                        <Text style={styles.messegeText}>Filter</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <ScrollView
+                style={styles.messegesList}
+                ref={ref => scrollWiew.current = ref}
+                onContentSizeChange={() => scrollWiew.current.scrollToEnd({ animated: true })}
+            >
+                <FlatList
+                    data={messages}
+                    renderItem={({ item, index }) =>
+                        <Message key={index}
+                            time={item.time}
+                            isLeft={item.user !== user.current}
+                            message={item.content}
+                        />}
+                    keyExtractor={(_, index) => index}
+                />
+            </ScrollView>
+            <View style={styles.newMessage}>
+                <View style={styles.inputBox}>
+                    <TextInput
+                        name="messages"
+                        placeholder="Message here..."
+                        placeholderTextColor="#BDBDBD"
+                        style={styles.input}
+                        variant="standard"
+                        onChangeText={value => setValue(value)}
+                        value={value}
+                        keyboardType="twitter"
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        secureTextEntry={false}
+                    />
+                </View>
+                <View style={styles.sendBox}>
+                    <TouchableOpacity onPress={getMessage}>
+                        <View style={styles.send} >
+                            <ArrowIcon
+                                width={16}
+                                height={25}
+                                fill='#FFFFFF'
+                            />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View >
+    )
+}
+
+export default MessagesList;
