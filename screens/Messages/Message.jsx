43427a959b485ba1cbf5290ae76a788e58f3c3f6
MiqayelChaloyan <miqochaloyan@gmail.com>
2023-05-15 14:44:42 +0400

    I removed the function from the Message file and replaced it with a ternary operator

--- screens/Messages/Message.jsx
+++ screens/Messages/Message.jsx
@@ -3,31 +3,11 @@ import styles from './style';
 
 
 const Message = ({ isLeft, message }) => {
-
-    const isOnLeft = (type) => {
-        if (isLeft && type === "messageContainer") {
-            return {
-                alignSelf: "flex-start",
-                backgroundColor: "#E8E8E8",
-                borderBottomLeftRadius: 0,
-                left: 20
-            };
-        } else if (isLeft && type === "message") {
-            return {
-                color: "#000",
-            };
-        } else {
-            return {
-                borderBottomRightRadius: 0,
-            };
-        }
-    };
-
     return (
         <View style={styles.container}>
-            <View style={[styles.messageContainer, isOnLeft('messageContainer')]}>
+            <View style={[isLeft ? styles.messageContainerisRight : styles.messageContainerisLeft]}>
                 <View style={styles.messageView}>
-                    <Text style={[styles.message, isOnLeft('message')]}>{message}</Text>
+                    <Text style={[isLeft ? styles.messageisRight : styles.messageIsLeft]}>{message}</Text>
                 </View>
             </View>
         </View>
