e5f6cccf38f3305f6130c6183d476283069a018d
Ando <andmkhart77@gmail.com>
2023-05-17 10:05:24 +0400

    Edited expenses data

--- Data/data.js
+++ Data/data.js
@@ -90,36 +90,36 @@ function DNAdata({children}) {
 
   const expensesData = [
     {
-      item: 'item1',
+      title: 'item1',
       price: 15,
     },
     {
-      item: 'item2',
-      price: 8,
+      title: 'item2',
+      price: 30,
     },
     {
-      item: 'item3',
+      title: 'item3',
       price: 28,
     },
     {
-      item: 'item4',
+      title: 'item4',
       price: 35,
     },
     {
-      item: 'item5',
+      title: 'item5',
       price: 12,
     },
     {
-      item: 'item6',
+      title: 'item6',
       price: 48,
     },
     {
-      item: 'item7',
+      title: 'item7',
       price: 32,
     },
     {
-      item: 'item8',
-      price: 12,
+      title: 'item8',
+      price: 25,
     },
   ];
 
