8863ec1f268a215172d8865652f37a8e38afb51a
Ando <andmkhart77@gmail.com>
2023-05-18 10:57:15 +0400

    Added Outlet list in ContentScreen

---
+++ screens/Content/ContentItemList.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Text, TouchableOpacity, View } from "react-native";
+import styles from "./style";
+import BuyIcon from "../../assets/icons/buy.svg";
+import { useNavigation } from "@react-navigation/native";
+
+const ContentItemList = ({ item, index }) => {
+  const navigation = useNavigation();
+  let backgroundColorStyle = index % 2 === 1 ? "#C6C5C5" : "#9B9A9A";
+  return (
+    <View style={styles.contentItemOfList}>
+      <View style={styles.leftSide}>
+        <View style={[styles.circle, { backgroundColor: backgroundColorStyle }]} />
+
+        <View style={styles.titleContainer}>
+          <Text style={styles.itemTitle}>{item.title}</Text>
+        </View>
+
+      </View>
+
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate({
+            name: "Feed",
+            params: { index },
+            merge: true,
+          });
+        }}>
+        <View>
+          <BuyIcon width={45} height={45} fill="#5DB075" />
+        </View>
+      </TouchableOpacity>
+
+    </View>
+
+  );
+};
+
+ContentItemList.propTypes = {
+  item: PropTypes.object,
+  index: PropTypes.number,
+};
+
+export default ContentItemList;
