c558bc34da8b8698c26ddad07c3b4ca66d52b9f4
MiqayelChaloyan <108179437+MiqayelChaloyan@users.noreply.github.com>
2023-05-23 10:34:43 +0400

    Changed page styles. (#28)
     Changed login and sign up page styles.

--- components/Header/style.js
+++ components/Header/style.js
@@ -1,8 +1,10 @@
 import {StyleSheet} from 'react-native';
+import {horizontalScale} from '../../assets/metrics/Metrics';
 import {theme} from '../../assets/theme/theme';
 
 const styles = StyleSheet.create({
   headerContainer: {
+    width: '100%',
     marginTop: 32,
     flexDirection: 'row',
     alignItems: 'center',
@@ -14,8 +16,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: theme.colors.foreground,
     lineHeight: 36.31,
-    maxWidth: 94,
-    maxHeight: 36,
+    maxWidth: horizontalScale(260),
     // TODO: This part needs to be updated.
     ...theme.textVariants.body,
   },
@@ -24,7 +25,6 @@ const styles = StyleSheet.create({
     fontWeight: 500,
     lineHeight: 19.36,
     maxWidth: 64,
-    maxHeight: 19,
     // TODO: This part needs to be updated.
     ...theme.textVariants.body,
   },
--- screens/authentication/login/style.js
+++ screens/authentication/login/style.js
@@ -1,8 +1,10 @@
-import {Dimensions, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {theme} from '../../../assets/theme/theme';
-
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+import {
+  horizontalScale,
+  moderateScale,
+  verticalScale,
+} from '../../../assets/metrics/Metrics';
 
 const styles = StyleSheet.create({
   loginRoot: {
@@ -13,149 +15,133 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   headerContainer: {
-    height: 14,
-    marginTop: 14,
-    marginBottom: 17,
+    height: verticalScale(14),
+    marginTop: verticalScale(14),
+    marginBottom: verticalScale(17),
   },
   header: {
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'stretch',
-    marginTop: 15,
-    marginHorizontal: 20,
+    marginTop: verticalScale(15),
+    marginHorizontal: horizontalScale(20),
   },
   cancel: {
-    top: 8,
-    height: 50,
-    width: windowWidth * 0.01,
+    top: verticalScale(8),
+    height: verticalScale(50),
+    width: horizontalScale(10),
   },
   headerBox: {
-    width: windowWidth * 0.79,
-    height: windowHeight * 0.1,
+    width: horizontalScale(300),
+    height: verticalScale(50),
   },
   logIn: {
-    fontSize: 30,
+    fontSize: moderateScale(30),
     fontWeight: 600,
     color: theme.colors.foreground,
     textAlign: 'center',
-    lineHeight: 36.31,
-    left: 10,
+    lineHeight: verticalScale(36.31),
+    left: verticalScale(10),
     // TODO: This part needs to be updated.
     ...theme.textVariants.body,
   },
   login: {
-    fontSize: 16,
+    fontSize: moderateScale(16),
     color: theme.colors.green,
-    lineHeight: 36.31,
+    lineHeight: verticalScale(36.31),
     fontWeight: 500,
-    width: windowWidth * 0.3,
-  },
-  passworddBox: {
-    width: windowWidth,
-  },
-  passHide: {
-    position: 'relative',
   },
   form: {
     justifyContent: 'center',
     alignItems: 'center',
-    gap: 15,
+    // gap: verticalScale(150),
   },
   hidePassword: {
-    fontSize: 16,
+    fontSize: moderateScale(16),
     color: theme.colors.green,
     fontWeight: 500,
     // TODO: This part needs to be updated.
     ...theme.textVariants.body,
   },
-  checkBox: {
-    padding: 15,
-    justifyContent: 'flex-start',
-    margin: 5,
-  },
   inputsContainer: {
-    width: windowWidth,
-    marginTop: '20%',
-    gap: 15,
-  },
-  nameInputStyle: {
-    width: windowWidth,
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: horizontalScale(375),
+    marginTop: verticalScale(64),
+    gap: moderateScale(16),
   },
   emailInputStyle: {
-    width: windowWidth,
+    width: horizontalScale(375),
     justifyContent: 'center',
     alignItems: 'center',
   },
   passwordInputStyle: {
-    width: windowWidth,
+    width: horizontalScale(375),
     justifyContent: 'center',
     alignItems: 'center',
     position: 'absolute',
   },
   forgotPassText: {
-    fontSize: 16,
+    fontSize: moderateScale(16),
     color: theme.colors.green,
-    lineHeight: 19.36,
+    lineHeight: verticalScale(19.36),
     // TODO: This part needs to be updated.
     ...theme.textVariants.body,
   },
   input: {
-    width: windowWidth * 0.9,
-    height: 51,
+    width: horizontalScale(343),
+    height: verticalScale(50),
     borderWidth: 1,
     borderColor: theme.colors.gray,
-    padding: 16,
+    padding: verticalScale(16),
     borderRadius: 8,
     backgroundColor: theme.colors.darkGray,
-    paddingHorizontal: 15,
-    paddingVertical: 7,
-    fontSize: 15,
+    paddingHorizontal: verticalScale(15),
+    paddingVertical: verticalScale(7),
+    fontSize: moderateScale(15),
     // TODO: This part needs to be updated.
     ...theme.textVariants.body,
   },
   inputError: {
     color: theme.colors.danger,
-    fontSize: 11,
-    marginTop: 7,
-    marginBottom: -10,
+    fontSize: moderateScale(11),
+    marginTop: verticalScale(7),
+    marginBottom: verticalScale(-10),
     // TODO: This part needs to be updated.
     ...theme.textVariants.body,
   },
   button: {
-    marginTop: '53%',
-    width: windowWidth * 0.9,
-    height: 55,
+    marginTop: verticalScale(183),
+    width: horizontalScale(343),
+    height: verticalScale(51),
     backgroundColor: theme.colors.green,
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 100,
+    borderRadius: verticalScale(100),
   },
   buttonText: {
     textAlign: 'center',
-    fontSize: 16,
+    fontSize: moderateScale(16),
     color: theme.colors.primary,
   },
   visibilityBtn: {
     position: 'absolute',
-    right: 30,
-    height: 25,
-    width: windowWidth * 0.12,
+    right: verticalScale(30),
+    height: verticalScale(25),
+    width: horizontalScale(40),
     padding: 0,
-    marginTop: 13,
+    marginTop: verticalScale(13),
   },
   forgotPass: {
     alignItems: 'center',
-    marginTop: 16,
+    marginTop: verticalScale(16),
   },
   navigateSignUp: {
     alignItems: 'center',
-    marginTop: 10,
+    marginTop: verticalScale(10),
   },
   loginFooter: {
     alignItems: 'center',
-    marginTop: '7%',
+    marginTop: verticalScale(7),
+    bottom: verticalScale(13.5),
   },
 });
 
