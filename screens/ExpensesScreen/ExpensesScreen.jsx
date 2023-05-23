bb92e562a03d2fff3913279b142252dc89353548
Ando <andmkhart77@gmail.com>
2023-05-23 09:52:31 +0400

    Edited ExpensesScreen styles using Metrics.js

--- screens/ExpensesScreen/ExpensesScreen.jsx
+++ screens/ExpensesScreen/ExpensesScreen.jsx
@@ -1,48 +1,51 @@
-import {useContext} from 'react';
-import {FlatList, Text, View} from 'react-native';
-import Header from '../../components/Header/Header';
-import {DNAdataContext} from '../../Data/data';
-import ExpenseItem from './ExpenseItem';
-import ExpenseItemList from './ExpenseItemList';
-import styles from './style';
+import { useContext } from "react";
+import { FlatList, Text, View } from "react-native";
+import Header from "../../components/Header/Header";
+import { DNAdataContext } from "../../Data/data";
+import ExpenseItem from "./ExpenseItem";
+import ExpenseItemList from "./ExpenseItemList";
+import styles from "./style";
 
-const ExpensesScreen = ({navigation}) => {
-  const {expensesData} = useContext(DNAdataContext);
+const ExpensesScreen = ({ navigation }) => {
+  const { expensesData } = useContext(DNAdataContext);
 
   return (
-    <View style={styles.container}>
-      <Header
-        // TODO: This part is will increase later.
-        screen={'Expenses'}
-        navigation={navigation}
-        back={'Market'}
-        continueTo={'Market'}
-        left={'Back'}
-        right={'New'}
-      />
-      <View style={styles.barChartContainer}>
-        <View style={styles.itemsContainer}>
+    <View style={styles.expenses}>
+
+      <View style={styles.container}>
+        <Header
+          // TODO: This part is will increase later.
+          screen={"Expenses"}
+          navigation={navigation}
+          back={"Market"}
+          continueTo={"Market"}
+          left={"Back"}
+          right={"New"}
+        />
+        <View style={styles.barChartContainer}>
+          <View style={styles.itemsContainer}>
+            <FlatList
+              horizontal
+              data={expensesData}
+              keyExtractor={(item, index) => index.toString() + item.title}
+              renderItem={({ item, index }) => {
+                return <ExpenseItem item={item} index={index} />;
+              }}
+            />
+          </View>
+        </View>
+        <View style={styles.headerTextContainer}>
+          <Text style={styles.headerText}>Expenses</Text>
+        </View>
+        <View style={styles.itemListContainer}>
           <FlatList
-            horizontal
             data={expensesData}
-            keyExtractor={(item, index) => index.toString() + item.title}
-            renderItem={({item, index}) => {
-              return <ExpenseItem item={item} index={index} />;
+            keyExtractor={(_, index) => index.toString()}
+            renderItem={({ item, index }) => {
+              return <ExpenseItemList item={item} index={index} />;
             }}
           />
         </View>
-      </View>
-      <View style={styles.headerTextContainer}>
-        <Text style={styles.headerText}>Expenses</Text>
-      </View>
-      <View style={styles.itemListContainer}>
-        <FlatList
-          data={expensesData}
-          keyExtractor={(_, index) => index.toString()}
-          renderItem={({item, index}) => {
-            return <ExpenseItemList item={item} index={index} />;
-          }}
-        />
       </View>
     </View>
   );
