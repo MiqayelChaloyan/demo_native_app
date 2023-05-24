58c57bfd740ff56e406500426011b597268013cc
Ando <andmkhart77@gmail.com>
2023-05-24 18:28:22 +0400

    Added new item for marketData

--- Data/context.js
+++ Data/context.js
@@ -66,11 +66,18 @@ function GlobalData({children}) {
       id: '3',
     },
     {
+      title: 'FORUM LOW SHOES',
+     imageUri:
+        'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/c2547ff40b3f47818118aed2015e64c6_9366/Forum_Low_Shoes_Black_FZ5891_01_standard.jpg',
+      price: '$19.99',
+      id: '4',
+    },
+    {
       title: 'GAZELLE SHOES',
       imageUri:
         'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/b0fd05cae60a48a6bd20c800cb0b8636_9366/Gazelle_Shoes_Blue_IG0666_HM1.jpg',
       price: '$19.99',
-      id: '4',
+      id: '5',
     },
   ]);
 
