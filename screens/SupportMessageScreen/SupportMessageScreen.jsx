93977f54737a2f4bb03affc0d7ee1f9198039c86
Ando <andmkhart77@gmail.com>
2023-05-27 17:02:37 +0400

    Implemented functionality for sending a message to the support using react-native-share

--- screens/SupportMessageScreen/SupportMessageScreen.jsx
+++ screens/SupportMessageScreen/SupportMessageScreen.jsx
@@ -3,8 +3,33 @@ import PropTypes from 'prop-types';
 import {Text, TextInput, TouchableOpacity, View} from 'react-native';
 import Header from '../../components/Header/Header';
 import styles from './style';
+import Share from 'react-native-share';
+
 const SupportMessageScreen = ({navigation}) => {
   const [text, setText] = useState('');
+
+  const share = async () => {
+    const options = {
+      message: text,
+      title: 'Testing the new app',
+      email: 'cs-reply@amazon.com',
+      subject: 'This is an example share.',
+      recipient: '+37496403050',
+    };
+
+    try {
+      const result = await Share.open(options);
+      if (result.action === Share.sharedAction) {
+        console.log('Share was successful');
+      } else if (result.action === Share.dismissedAction) {
+        console.log('Share was dismissed');
+      } else if (result.action === Share.failedAction) {
+        console.log('Share failed');
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
   return (
     <View style={styles.supportScreenContainer}>
       <View style={styles.supportScreen}>
@@ -30,7 +55,7 @@ const SupportMessageScreen = ({navigation}) => {
       </View>
       <View style={styles.buttonContainer}>
         <View style={styles.button}>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={share}>
             <Text style={styles.buttonText}>Send</Text>
           </TouchableOpacity>
         </View>
