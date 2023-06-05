a6be4eb99eca24094dfbff4a2b2bac258f4b2807
Ando <andmkhart77@gmail.com>
2023-06-05 19:19:20 +0400

    Edited Market screen

---
+++ screens/MarketScreen/Chapter.jsx
@@ -0,0 +1,25 @@
+import {Text, TouchableOpacity, View} from 'react-native';
+import VideoIcon from '../../assets/icons/Video.svg';
+import {horizontalScale, verticalScale} from '../../assets/metrics/Metrics';
+import {theme} from '../../assets/theme/theme';
+import MarketItemList from './MarketItemList';
+import styles from './style';
+
+const Chapter = ({data, navigation, header}) => {
+  return (
+    <View>
+      <View style={styles.hotDealsContainer}>
+        <Text style={styles.hotDealsText}>{header}</Text>
+        <TouchableOpacity style={styles.playButton}>
+          <VideoIcon
+            width={horizontalScale(30)}
+            height={verticalScale(30)}
+            fill={theme.colors.black}
+          />
+        </TouchableOpacity>
+      </View>
+      <MarketItemList data={data} navigation={navigation} />
+    </View>
+  );
+};
+export default Chapter;
