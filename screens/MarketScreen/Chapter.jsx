5f9024074612906110a1458538504d78d2b58be2
Andranik Mkhitaryan <andmkhart77@gmail.com>
2023-06-06 10:25:56 +0400

    Edited Market screen (#48)
    Edited Market screen

---
+++ screens/MarketScreen/Chapter.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import {Text, TouchableOpacity, View} from 'react-native';
+import VideoIcon from '../../assets/icons/Video.svg';
+import {horizontalScale, verticalScale} from '../../assets/metrics/Metrics';
+import {theme} from '../../assets/theme/theme';
+import MarketItemList from './MarketItemList';
+import styles from './style';
+
+const Chapter = ({data, navigation, header}) => {
+  return (
+    <View>
+      <View style={styles.hotDealsContainer}>
+        <Text style={styles.hotDealsText}>{header}</Text>
+        <TouchableOpacity style={styles.playButton}>
+          <VideoIcon
+            width={horizontalScale(30)}
+            height={verticalScale(30)}
+            fill={theme.colors.black}
+          />
+        </TouchableOpacity>
+      </View>
+      <MarketItemList data={data} navigation={navigation} />
+    </View>
+  );
+};
+
+Chapter.propTypes = {
+  navigation: PropTypes.object,
+  data: PropTypes.object,
+  header: PropTypes.string,
+};
+export default Chapter;
--- screens/MarketScreen/index.js
+++ screens/MarketScreen/index.js
@@ -1,12 +1,10 @@
 import PropTypes from 'prop-types';
 import {useEffect, useState} from 'react';
-import {Text, View, ScrollView, TouchableOpacity} from 'react-native';
-import {horizontalScale, verticalScale} from '../../assets/metrics/Metrics';
-import MarketItemList from './MarketItemList';
+import {Text, View, ScrollView} from 'react-native';
+import Chapter from './Chapter';
 import Header from '../../components/Header/Header';
 import Search from '../../components/Search/Search';
-import VideoIcon from '../../assets/icons/Video.svg';
-import {theme} from '../../assets/theme/theme';
+
 import {getDataMarketFromFile} from '../../utils/ApiUtils';
 import styles from './style';
 
@@ -37,39 +35,9 @@ const MarketScreen = ({navigation}) => {
       </View>
       {state.length ? (
         <View style={styles.contentContainer}>
-          <View style={styles.hotDealsContainer}>
-            <Text style={styles.hotDealsText}>Hot deals</Text>
-            <TouchableOpacity style={styles.playButton}>
-              <VideoIcon
-                width={horizontalScale(30)}
-                height={verticalScale(30)}
-                fill={theme.colors.black}
-              />
-            </TouchableOpacity>
-          </View>
-          <MarketItemList data={state} navigation={navigation} />
-          <View style={styles.hotDealsContainer}>
-            <Text style={styles.hotDealsText}>Trending</Text>
-            <TouchableOpacity style={styles.playButton}>
-              <VideoIcon
-                width={horizontalScale(30)}
-                height={verticalScale(30)}
-                fill={theme.colors.black}
-              />
-            </TouchableOpacity>
-          </View>
-          <MarketItemList data={state} navigation={navigation} />
-          <View style={styles.hotDealsContainer}>
-            <Text style={styles.hotDealsText}>Deals</Text>
-            <TouchableOpacity style={styles.playButton}>
-              <VideoIcon
-                width={horizontalScale(30)}
-                height={verticalScale(30)}
-                fill={theme.colors.black}
-              />
-            </TouchableOpacity>
-          </View>
-          <MarketItemList data={state} navigation={navigation} />
+          <Chapter data={state} navigation={navigation} header="Hot Deals" />
+          <Chapter data={state} navigation={navigation} header="Trending" />
+          <Chapter data={state} navigation={navigation} header="Deals" />
         </View>
       ) : (
         <View style={styles.warning}>
